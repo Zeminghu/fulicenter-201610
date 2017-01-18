ce5784337d9633a3369f7aa8a2a34fe22474ad80
Hu Zeming <1059434300@qq.com>
2017-01-18 21:48:42 +0800

    商品详情收藏商品的操作

--- app/src/main/java/cn/ucai/fulicenter/model/net/IModelUser.java
+++ app/src/main/java/cn/ucai/fulicenter/model/net/IModelUser.java
@@ -2,6 +2,8 @@ package cn.ucai.fulicenter.model.net;
 
 import android.content.Context;
 
+import java.io.File;
+
 import cn.ucai.fulicenter.controller.activity.LoginActivity;
 import cn.ucai.fulicenter.model.bean.NewGoodsBean;
 import cn.ucai.fulicenter.model.bean.User;
@@ -12,11 +14,13 @@ import cn.ucai.fulicenter.model.utils.OkHttpUtils;
  */
 
 public interface IModelUser {
-    void login(Context context, String username,String password, OnCompleteListener<String> listener);
-    void register(Context context, String username, String usernick, String password,OnCompleteListener<String> listener);
-    void updatNick(Context context, String username, String usernick,OnCompleteListener<String> listener);
+    void login(Context context, String username, String password, OnCompleteListener<String> listener);
 
+    void register(Context context, String username, String usernick, String password, OnCompleteListener<String> listener);
 
+    void updatNick(Context context, String username, String usernick, OnCompleteListener<String> listener);
+
+    void uploadAvatar(Context context, String username, File file, OnCompleteListener<String> listener);
 
 
 }
